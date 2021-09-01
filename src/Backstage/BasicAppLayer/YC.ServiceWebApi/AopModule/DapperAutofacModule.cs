da65ad3a7c22c68f77ac558ee7f2f3debf5d4c05
linxuanming <2594771947@qq.com>
2021-09-01 16:53:11 +0800

    添加sqlserver的支持

--- src/Backstage/BasicAppLayer/YC.ServiceWebApi/AopModule/DapperAutofacModule.cs
+++ src/Backstage/BasicAppLayer/YC.ServiceWebApi/AopModule/DapperAutofacModule.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using Autofac.Extras.DynamicProxy;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Caching.Redis;
 using System;
@@ -21,16 +22,30 @@ namespace YC.ServiceWebApi.AopModule
     {
         protected override void Load(ContainerBuilder builder)
         {
-          
+
             builder.RegisterGeneric(typeof(RepositoryBase<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope().EnableInterfaceInterceptors()
                      .InterceptedBy(typeof(AopInterceptor));//这一步是仓储注入的重要的点，允许拦截器
-           
+
             builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly());//注入当前程程序集
-            // builder.RegisterType<DefaultUnitOfWork>().As<IUnitOfWork>().WithParameter("dbConnectionString",dbConfig.DefaultDbConnectionString).AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired();
-         
-            builder.RegisterType<DefaultUnitOfWork>().As<DapperFrameWork.IUnitOfWork>().AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired();
+                                                                           // builder.RegisterType<DefaultUnitOfWork>().As<IUnitOfWork>().WithParameter("dbConnectionString",dbConfig.DefaultDbConnectionString).AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired();
+
+            var dbType =
+        builder.RegisterType<DefaultUnitOfWork>().As<DapperFrameWork.IUnitOfWork>().WithParameter("dbConnectionString", DefaultConfig.TenantSettingDto.DefaultDbConnectionString).WithParameter("dbtype", GetDbType()).AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired();
             //builder.RegisterType<TenantIdentificationStrategy>().As<ITenantIdentificationStrategy>().AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired();
 
+
+        }
+
+        private static RepositoryUtils.Dialect GetDbType()
+        {
+            RepositoryUtils.Dialect dbType;
+            switch (DefaultConfig.TenantSettingDto.DataType)
+            {
+                case 0: dbType = RepositoryUtils.Dialect.MySQL; break;
+                case 1: dbType = RepositoryUtils.Dialect.SQLServer; break;
+                default: dbType = RepositoryUtils.Dialect.SQLServer; break;
+            }
+            return dbType;
 
         }
     }
