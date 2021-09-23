dcf4f69c66898f114656344e19e898d5b447f04b
linxuanming <2594771947@qq.com>
2021-09-23 14:00:29 +0800

    删除部分冗余代码，添加自定义初始化任务

---
+++ src/Backstage/BasicAppLayer/YC.ServiceWebApi/WebHostBuilderExtensions/InitHostingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YC.Common;
+
+namespace YC.ServiceWebApi.WebHostBuilderExtensions
+{
+    public static class InitHostingExtensions
+    {
+        /// <summary>
+        /// 自定义初始化相关的附加任务
+        /// </summary>
+        /// <param name="hostBuilder"></param>
+        /// <param name="test"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder UseInitCall(this IWebHostBuilder hostBuilder, Action action) {
+
+            action?.Invoke();
+            //action?.BeginInvoke();处理异步
+            return hostBuilder;
+        }
+
+        public static void Call(string info) {
+
+            //custom work
+            //LogUtils.WriteLog(new LogDto() { TypeName = "TestLog", Message = info });
+        }
+    }
+
+   
+
+   
+}
