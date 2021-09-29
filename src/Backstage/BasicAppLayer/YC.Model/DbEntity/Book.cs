e7ce08d5a1eb671834722ee16be70c7b64d1df7d
linxuanming <2594771947@qq.com>
2021-09-29 18:41:54 +0800

    新增 elasticSearch 模块

---
+++ src/Backstage/BasicAppLayer/YC.Model/DbEntity/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YC.Model.DbEntity
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        
+        public string BookName { get; set; }
+
+        public string BookContent { get; set; }
+        public string Auther { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        public double Price { get; set; }
+    }
+}
---
+++ src/Backstage/Module/YC.ElasticSearch/IElasticSearchRepository.cs
@@ -0,0 +1,16 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YC.ElasticSearch
+{
+   public interface IElasticSearchRepository<T> where T : class, new()
+    {
+        Task<IndexResponse> CreateAsync(T input);
+        Task<BulkResponse> CreateListAsync(List<T> input);
+        Task<MultiGetResponse> MultiGetAsync();
+    }
+}
