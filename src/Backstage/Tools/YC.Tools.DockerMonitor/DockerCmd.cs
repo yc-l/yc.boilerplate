b81659c0152f0c10b6f13b5478b661e517c4237d
linxuanming <2594771947@qq.com>
2021-11-05 15:18:07 +0800

    新增docker版本和监控操作工具

---
+++ src/Backstage/Tools/YC.Tools.DockerMonitor/DockerCmd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YC.Tools.DockerMonitor
+{
+  public  class DockerCmd
+    {
+        /// <summary>
+        ///显示docker 监控
+        /// </summary>
+        /// <param name="dockerContainId"></param>
+        /// <returns></returns>
+        public static string ContainsStats(string dockerContainId)=>$"docker stats -a  {dockerContainId}";
+
+        /// <summary>
+        /// 显示docker内部日志
+        /// </summary>
+        /// <param name="dockerContainId"></param>
+        /// <returns></returns>
+        public static string ContainsLogs(string dockerContainId) => $"docker logs  {dockerContainId}";
+    }
+}
---
+++ src/Backstage/Tools/YC.Tools.DockerMonitor/Model/ConfigModel/SshConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YC.Tools.DockerMonitor
+{
+   public class SshConfig
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string Url {
+
+            get { 
+            return this.Host+":"+this.Port;
+            }
+        }
+
+        public string Title {
+            get
+            {
+                return this.UserName + "@" + this.Url;
+            }
+
+        }
+    }
+}
